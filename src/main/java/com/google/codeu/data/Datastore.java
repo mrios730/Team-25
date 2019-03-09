c22e1334abf6e70a14ac12615dbf3665a637b61d
mojuwara <mj1803@nyu.edu>
2019-03-09 14:54:23 -0500

    Merge issues fixed

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -127,7 +127,7 @@ public class Datastore {
   /**
     * Returns the User owned by the email address, or
     * null if no matching User was found.
-    */
+	 **/
   public User getUser(String email) {
     Query query = new Query("User")
         .setFilter(new Query.FilterPredicate("email", FilterOperator.EQUAL, email));
