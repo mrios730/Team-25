19c5abdf0c307224e6dcab0edd2f60a42936835e
mojuwara <mj1803@nyu.edu>
2019-03-20 22:49:45 -0400

    Formatting issues

--- src/main/java/com/google/codeu/servlets/MapServlet.java
+++ src/main/java/com/google/codeu/servlets/MapServlet.java
@@ -1,61 +1,58 @@
 package com.google.codeu.servlets;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import java.io.IOException;
 import java.util.Scanner;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 
-/**
- * Returns Starbucks locations data as a JSON array.
- */
+/** Returns Starbucks locations data as a JSON array. */
 @WebServlet("/starbucks-map")
 public class MapServlet extends HttpServlet {
 
-	JsonArray starbucksArray;
+  JsonArray starbucksArray;
 
-	@Override
-	public void init() {
-		starbucksArray = new JsonArray();
-		Gson gson = new Gson();
-		Scanner scanner = new Scanner(getServletContext()
-				.getResourceAsStream("/WEB-INF/starbucks-directory.csv"));
-		//Skip the first line (Column names)
-		scanner.nextLine();
-		while(scanner.hasNextLine()) {
-			String line = scanner.nextLine();
-			String[] cells = line.split(",");
+  @Override
+  public void init() {
+    starbucksArray = new JsonArray();
+    Gson gson = new Gson();
+    Scanner scanner =
+        new Scanner(getServletContext().getResourceAsStream("/WEB-INF/starbucks-directory.csv"));
+    // Skip the first line (Column names)
+    scanner.nextLine();
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] cells = line.split(",");
 
-			//System.out.println(cells[0] + ", " + cells[1] + ", " + cells[2]);
+      // System.out.println(cells[0] + ", " + cells[1] + ", " + cells[2]);
 
-			String store_num = cells[0];
-			double lat = Double.parseDouble(cells[2]);
-			double lng = Double.parseDouble(cells[1]);
+      String store_num = cells[0];
+      double lat = Double.parseDouble(cells[2]);
+      double lng = Double.parseDouble(cells[1]);
 
-			starbucksArray.add(gson.toJsonTree(new Starbucks(store_num, lat, lng)));
+      starbucksArray.add(gson.toJsonTree(new Starbucks(store_num, lat, lng)));
+    }
+    scanner.close();
+  }
 
-		}
-		scanner.close();
-	}
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    response.getOutputStream().println(starbucksArray.toString());
+  }
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.setContentType("application/json");
-		response.getOutputStream().println(starbucksArray.toString());
-	}
+  private static class Starbucks {
+    String store_num;
+    double lat;
+    double lng;
 
-	private static class Starbucks{
-		String store_num;
-		double lat;
-		double lng;
-
-		private Starbucks(String name, double lat, double lng) {
-			this.store_num = name;
-			this.lat = lat;
-			this.lng = lng;
-		}
-	}
+    private Starbucks(String name, double lat, double lng) {
+      this.store_num = name;
+      this.lat = lat;
+      this.lng = lng;
+    }
+  }
 }
