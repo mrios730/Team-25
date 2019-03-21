246b8d6d8c3de73bc4ff3b033631faa3e807231d
mojuwara <mj1803@nyu.edu>
2019-03-20 23:19:59 -0400

    Changed some comments and added some some more comments

--- src/main/java/com/google/codeu/servlets/MapServlet.java
+++ src/main/java/com/google/codeu/servlets/MapServlet.java
@@ -12,39 +12,44 @@ import javax.servlet.http.HttpServletResponse;
 /** Returns Starbucks locations data as a JSON array. */
 @WebServlet("/starbucks-map")
 public class MapServlet extends HttpServlet {
-
   JsonArray starbucksArray;
 
-  @Override
+	/**
+	 * init() will run once, creating and storing Starbucks objects with data from the
+	 * CSV file when the servlet is initialized.
+	 */
+	@Override
   public void init() {
     starbucksArray = new JsonArray();
     Gson gson = new Gson();
-    Scanner scanner =
-        new Scanner(getServletContext().getResourceAsStream("/WEB-INF/starbucks-directory.csv"));
+    Scanner scanner = new Scanner(getServletContext()
+				.getResourceAsStream("/WEB-INF/starbucks-directory.csv"));
     // Skip the first line (Column names)
     scanner.nextLine();
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine();
       String[] cells = line.split(",");
-
-      // System.out.println(cells[0] + ", " + cells[1] + ", " + cells[2]);
-
       String storeNumber = cells[0];
       double lat = Double.parseDouble(cells[2]);
       double lng = Double.parseDouble(cells[1]);
-
       starbucksArray.add(gson.toJsonTree(new Starbucks(storeNumber, lat, lng)));
     }
     scanner.close();
   }
 
+	/**
+	 * 	Send the json data when the user goes to "/starbucks-map".
+	 */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
     response.getOutputStream().println(starbucksArray.toString());
   }
 
-  private static class Starbucks {
+	/**
+	 * Starbucks class that allows each Starbucks to store its own info.
+	 */
+	private static class Starbucks {
     String storeNumber;
     double lat;
     double lng;
